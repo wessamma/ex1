e42ac222dfb93a5f1b87df1eb68710ea5c1e53fa
wessamma <engbomokdob@gmail.com>
2022-08-10 10:58:33 +0200

    gg

--- exercise.py
+++ exercise.py
@@ -2,4 +2,57 @@
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
 
-print(234,'\n',43.12,'\n',
+
+#Task 1
+
+
+print('````bach')
+
+print(234)
+print(43.12)
+print(3+2j)
+print('Hello')
+print(True)
+
+print('````')
+
+#Task 2
+
+print('````bach')
+
+print(123,type(123),sep =' is type of ')
+print(43.23,type(43.22),sep =' is type of ')
+print(4-2j,type(4-2j),sep =' is type of ')
+print('How are you',type('Hello'),sep =' is type of ')
+
+x=True
+
+print(True,type(x),sep =' is type of ')
+
+print('````')
+
+#Task 3
+
+print('````bach')
+
+
+print(isinstance (123,int))
+print(isinstance (123,float))
+print(isinstance ('fff',str))
+print(isinstance (1+5j,str))
+
+print('````')
+
+#Task 4
+
+print('````bach')
+
+
+print('Is ',123,' an instance of int?: ',isinstance (123,int))
+print('Is ',43.5,' an instance of Bool?: ',isinstance (43.5,bool))
+print('Is ',4-4j,' an instance of complex?: ',isinstance (4-4j,complex))
+print('Is ',True,' an instance of Bool?: ',isinstance (True,bool))
+print('Is ',"'How are you'","an instance of float?: ",isinstance ('h',float))
+
+print('````')
+
